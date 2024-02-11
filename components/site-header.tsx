b3f6cc7e2ec22df669013ec8ccdaf0f87f214e88
刘垚 <liuyao.santurn@bytedance.com>
2024-02-11 17:07:41 +0800

    add head
    Change-Id: If288874a79a13de9700274f46f3d9460e8f58443

---
+++ components/site-header.tsx
@@ -0,0 +1,5 @@
+export function SiteHeader() {
+    return <head>
+
+    </head>
+}
---
+++ types/components_argument.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export type ChildrenOnly = Readonly<{
+    children: React.ReactNode
+}>
